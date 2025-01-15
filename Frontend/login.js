b80734d55acec5cc4983cc630aa45a08e9bfc69a
Ujjwal Reddy K S <66116582+ujjwalredd@users.noreply.github.com>
2025-01-15 06:45:16 -0500

    updated push

---
+++ Frontend/login.js
@@ -0,0 +1,46 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+import {getAuth, signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBBL1fqTEnpAajWJVlLz6U-2fLJHiOJCpM",
+  authDomain: "mane-4b919.firebaseapp.com",
+  projectId: "mane-4b919",
+  storageBucket: "mane-4b919.firebasestorage.app",
+  messagingSenderId: "751526870289",
+  appId: "1:751526870289:web:98d319f6eae2dac61f20b0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+document.addEventListener('DOMContentLoaded', () => {
+    const loginForm = document.getElementById('loginForm');
+    
+    if (loginForm) {
+        loginForm.addEventListener('submit', (e) => {
+            e.preventDefault();
+            const email = document.getElementById('email').value;
+            const password = document.getElementById('password').value;
+            
+            signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed up 
+                const user = userCredential.user;
+                window.location.href = 'http://127.0.0.1:8080/profile.html';
+                // ...
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                alert(errorMessage)
+                // ..
+            });
+        });
+
+    }
+});
